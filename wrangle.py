5104f7152ef0ac4775364b9fe4f87fdc4d7a6a9b
Peter Chavez <107886587+Peter-N-Chavez@users.noreply.github.com>
2022-08-30 14:36:05 -0500

    Updates to functions and visualizations.

--- wrangle.py
+++ wrangle.py
@@ -22,9 +22,131 @@ from prepare import *
 from explore import *
 import env
 
-def wrangle_zillow():
+def acquire_zillow():
     url_zillow = get_db_url(env.hostname, env.username, env.password, "zillow")
-    query = pd.read_sql('''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet AS cal_fin_sqf, taxvaluedollarcnt AS tax_val, yearbuilt, taxamount, fips \
+    query = pd.read_sql('''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet AS cal_fin_sqf, taxvaluedollarcnt AS tax_val, yearbuilt AS year_built, taxamount, fips \
                         FROM properties_2017 \
                         WHERE propertylandusetypeid = 261; ''', url_zillow)
     return query
+
+def remove_outliers_v2(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    # Create a column that will label our rows as containing an outlier value or not
+    num_obs = df.shape[0]
+    df['outlier'] = False
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # update the outlier label any time that the value is outside of boundaries
+        df['outlier'] = np.where(((df[col] < lower_bound) | (df[col] > upper_bound)) & (df.outlier == False), True, df.outlier)
+    
+    df = df[df.outlier == False]
+    df.drop(columns=['outlier'], inplace=True)
+    print(f"Number of observations removed: {num_obs - df.shape[0]}")
+        
+    return df
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    # removing outliers
+    df = remove_outliers_v2(df, 1.5, ['bedroomcnt', 'bathroomcnt', 'cal_fin_sqf', 'tax_val',           'taxamount'])
+    
+    # converting column datatypes
+    df.fips = df.fips.astype(object)
+    df.year_built = df.year_built.astype(object)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute year built using mode
+    imputer = SimpleImputer(strategy='median')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test    
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test
+
+def get_box(df):
+    ''' Gets boxplots of acquired continuous variables'''
+    
+    # List of columns
+    cols = ['bedroomcnt', 'bathroomcnt', 'cal_fin_sqf', 'tax_val', 'taxamount']
+
+    plt.figure(figsize=(16, 3))
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display boxplot for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # sets proper spacing between plots
+        plt.tight_layout()
+
+    plt.show()
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+
+def get_box_hist_viz(df):
+    get_hist(df)
+    get_box(df)
